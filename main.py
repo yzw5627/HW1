8dc86297141d042472cd5fbfcf5e4d2a2c183b3f
yzw5627 <yzw5627@psu.edu>
2020-08-28 01:11:42 +0000

    change int into float

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 C = input("Enter temperature in celsius: ")
+c = round(float(C), 2)
 F = (float(C)*9/5+32)
 
-print(str(C) + chr(176) +" in Celsius is equivalent to " + str(F) +  chr(176)+" Fahrenheit.")
+print(str(c) + chr(176) +" in Celsius is equivalent to " + str(F) +  chr(176)+" Fahrenheit.")
